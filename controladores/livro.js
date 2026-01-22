db3afe57c51cdc80d56a64b6d6fc79997bc55a22
Flávio Ghiraldeli Botelho De Paula <ghiraldeli26@gmail.com>
2026-01-22 13:54:42 -0300

    Adding a condition so the body is not null

--- controladores/livro.js
+++ controladores/livro.js
@@ -33,9 +33,15 @@ function getLivro(req, res) {
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send("livro inserido com sucesso")
+        if (req.body.nome) {
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("livro inserido com sucesso")
+        } else {
+            res.status(422)
+            res.send("O campo nome é obrigatório")
+        }
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -49,13 +55,13 @@ function patchLivro(req, res) {
         if (id && Number(id)) {
             const body = req.body
 
-        modificaLivro(body, id)
-        res.send("item modificado com sucesso")    
+            modificaLivro(body, id)
+            res.send("item modificado com sucesso")
         } else {
             res.status(422)
             res.send("Id inválido")
         }
-        
+
     } catch (error) {
         res.status(500)
         res.send(error.message)
